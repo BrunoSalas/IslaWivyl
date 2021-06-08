2c563416289168b6eeb2734d7ba888a81d077a96
Dulce <asnzkzn123@gmail.com>
2021-06-07 19:22:55 -0500

    prueba

---
+++ Assets/Scripts/Saludito.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saludito : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hola");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
