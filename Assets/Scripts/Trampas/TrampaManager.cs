d07e00c22f58b4433d45dabaaf27fb0c98462f10
JoseCabrera9608 <73959334+JoseCabrera9608@users.noreply.github.com>
2021-06-21 02:54:33 -0500

    Trampa1 (evento) Trampa3

--- Assets/Scripts/Trampas/TrampaManager.cs
+++ Assets/Scripts/Trampas/TrampaManager.cs
@@ -7,6 +7,8 @@ public class TrampaManager : MonoBehaviour
 {
     
     public int numeroTrampa = 0; //Modificar la variable desde el player por colision con cada trampa
+    //public GameObject[] coco;
+    //int i;
     void Start()
     {
         
@@ -24,6 +26,7 @@ public class TrampaManager : MonoBehaviour
         {
             case 1:
                 Debug.Log("trampa 1 activada");
+                //TrampaUno();
                 numeroTrampa = 0;
                 break;
 
@@ -34,11 +37,34 @@ public class TrampaManager : MonoBehaviour
 
             case 3:
                 Debug.Log("trampa 3 activada");
+                //TrampaTres();
                 numeroTrampa = 0;
                 break;
 
         }
     }
 
-    
+    /*void TrampaUno()
+    {
+        for (i = 0; i<= arboles.Length; i++)
+        {
+            GameObject temporalArbol = coco[i].GetComponent<RigidBody>();
+            temporalArbol.gravityScale = 9.81;
+        }
+    }*/
+
+    /*void TrampaTres()
+    {
+        IEnumerator Congelar() {
+        Vector3 velocidadLineal = rigidbody.velocity;
+        Vector3 velocidadAngular = rigidbody.angularVelocity;
+        rigidbody.velocity = Vector3.zero
+        rigidbody.angularVelocity = Vector3.zero;
+        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        
+        yield return new WaitForSeconds (4);
+        rigidbody.constraints = RigidbodyConstraints.None;
+        rigidbody.velocity = velocidadLineal;
+        rigidbody.angularVelocity = velocidadAngular;
+    */
 }
