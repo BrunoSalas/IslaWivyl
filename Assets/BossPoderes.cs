e4d11475d01a8b82d0713ddc4fcb6c8c3f36ab82
JoseCabrera9608 <73959334+JoseCabrera9608@users.noreply.github.com>
2021-07-16 02:49:14 -0500

    Boss-Poder de levantar Muro

--- Assets/BossPoderes.cs
+++ Assets/BossPoderes.cs
@@ -9,40 +9,49 @@ public class BossPoderes : MonoBehaviour
     public GameObject piedras;
     public float timer;
     public int i;
-    Vector3 impulso;
+    public AudioSource sonido;
+    public Animator animator;
     void Start()
     {
         trampas = GameObject.FindGameObjectsWithTag ("SpawnTrampas");
-        impulso = new Vector3 (0,5,0);
+        sonido = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer > 5)
-        {
-            EscogerSpawn ();
+     
 
             if (timer>6)
             {
                 timer = 0;
+                animator.SetTrigger ("Rugido");
+                sonido.Play();
                 CrearPiedras();
             }
-        }
+        
     }
 
 
     public void CrearPiedras (){
-        GameObject piedrasTemporales = Instantiate(piedras);
-        piedrasTemporales.transform.position = trampaEscogida.transform.position;
-        Vector3 piedrasTemp = piedrasTemporales.transform.position;
-        piedrasTemp += impulso;
+        //GameObject piedrasTemporales = Instantiate(piedras);
+        //piedrasTemporales.transform.position = trampaEscogida.transform.position;
+        for (i=0 ; i<trampas.Length; i++){
+            trampaEscogida = trampas [i];
+            GameObject piedrasTemporales = Instantiate(piedras);
+            piedrasTemporales.transform.position = trampaEscogida.transform.position;
+            Destroy(piedrasTemporales, 5f);
+        }
         
     }
 
     void EscogerSpawn  (){
-        i = Random.Range (0,trampas.Length);
-        trampaEscogida = trampas [i];
+        
+        
+        //trampaEscogida = trampas [0];
     }
 }
